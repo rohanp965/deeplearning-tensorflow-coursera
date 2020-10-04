c3480ac75757887b734f89ad3ed51316e55ce7f7
Rohan Pimprikar <rohanp965@gmail.com>
2020-10-03 20:31:36 -0400

    housing example

--- week1/linear-equation.py
+++ week1/linear-equation.py
@@ -1,4 +1,14 @@
-import pandas as pd
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
 
-print(pd.DataFrame({'column1':[1,2,3]}))
+def house_model(y_new):
+    xs = [1,2,3,4,5,6]
+    ys = [1,1.5,2,2.5,3,3.5]
+    model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    model.fit(xs, ys, epochs=500)
+    return model.predict(y_new)[0]
 
+prediction = house_model([7.0])
+print(prediction)
